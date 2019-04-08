7a62fd0a089556e29bf067eda4062b7a3f4c90dd
Charles Hardy <charlie.hardy@outlook.com>
2019-04-08 13:57:40 +0100

    Seperate ApplicationDBContext
    Remove Application DbContext in to own class

--- SwimmingAPI/Models/IdentityModels.cs
+++ SwimmingAPI/Models/IdentityModels.cs
@@ -28,19 +28,5 @@ namespace SwimmingAPI.Models
 
     }
 
-    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    {
-        public DbSet<Meet> Meets { get; set; }
-        public DbSet<Event> Events { get; set; }
-        public DbSet<EventResult> EventResults { get; set; } 
-        public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
-        {
-        }
-        
-        public static ApplicationDbContext Create()
-        {
-            return new ApplicationDbContext();
-        }
-    }
+   
 }
